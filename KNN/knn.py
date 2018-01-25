830c7aec95debe982db2754b3787dd716a245620
zxt50330 <zxt50330@gmail.com>
2018-01-25 15:36:54 +0800

    knn finished

--- KNN/knn.py
+++ KNN/knn.py
@@ -99,3 +99,42 @@ import matplotlib.pyplot as plt
 # plt.show()
 
 
+####### 手写数字 ######
+from os import listdir
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i + j] = int(lineStr[j])
+    return returnVect
+
+# print(img2vector('./digits/testDigits/0_13.txt'))
+
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('./digits/trainingDigits/')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('./digits/trainingDigits/' + fileNameStr)
+    testFileList = listdir('./digits/testDigits/')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('./digits/testDigits/' + fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        # print( "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
+handwritingClassTest()
